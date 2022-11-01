0cfcbd5193ffb88be108c9a0bdae9c169a4f1ac8
Misha Tuesday <mishatuesday@gmail.com>
2022-11-01 09:02:29 -0400

    commit tree 9e0752d9b9415dbdac67a92495104efbcb447295

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
 
 
+class Book
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_reader :title
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
